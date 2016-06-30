3646a80fc05cdc330bc57f9b56b6962e474d32eb
Weston Renoud <renoud@qps.nl>
2016-06-30 14:04:39 -0700

    use readme for package description

--- setup.py
+++ setup.py
@@ -5,6 +5,12 @@ from distutils.extension import Extension
 NAME = "cypyserialize"
 VERSION = "0.3"
 DESCR = "Really easy, really quick, binary parser framework for Python"
+try:
+   import pypandoc
+   LONG_DESC = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+   LONG_DESC = open('README.md').read()
+
 URL = "https://github.com/wrenoud/cypyserialize"
 DOWNLOAD_URL = "https://github.com/wrenoud/cypyserialize/tarball/" + VERSION
 
@@ -42,6 +48,7 @@ setup(
     packages=PACKAGES,
     version=VERSION,
     description=DESCR,
+    long_description=LONG_DESC,
     author=AUTHOR,
     author_email=EMAIL,
     url=URL,
